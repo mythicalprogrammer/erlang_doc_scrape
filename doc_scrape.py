0ef1b306654386962b1e4077987c8ce60f59e0aa
Anthony Doan <mythicalprogrammer@gmail.com>
2013-02-05 10:03:34 -0800

    Made a list of tuple for every app and an index lookup table for every category

--- doc_scrape.py
+++ doc_scrape.py
@@ -5,22 +5,31 @@ import os
 #app_list = os.listdir('otp_doc_html_R16A_RELEASE_CANDIDATE/lib')
 #app_list = [x.split('-')[0] for x in app_list]
 
-app_list = open('otp_doc_html_R16A_RELEASE_CANDIDATE/doc/applications.html','r')
-app_soup = BeautifulSoup(app_list).find_all('tr')
-app_cat = ''
+app_html = open('otp_doc_html_R16A_RELEASE_CANDIDATE/doc/applications.html','r')
+app_soup = BeautifulSoup(app_html).find_all('tr')
+app_category = ''
+app_list = []
+sql_app_id_iter = 0
+sql_app_id_lookup = {}
 for i in range(0, len(app_soup)):
     if len(app_soup[i].attrs) == 0:
         if len(app_soup[i].td.attrs) == 2:
-            app_cat = app_soup[i].td.font.b.text # App Category
+            app_category = app_soup[i].td.font.b.text.strip().encode('UTF-8') # App Category
+	    sql_app_id_iter += 1 
+	    sql_app_id_lookup[app_category] = sql_app_id_iter
    #         print app_soup[i].td.font.b.text # App Category
     elif len(app_soup[i].attrs) == 1:
         td_list = app_soup[i].find_all('td')
         if len(td_list) == 3:
-           print app_cat
+           #print app_cat
            a_list = td_list[1].find_all('a')
-           print a_list[0].text # App Name
-           print a_list[1].text # App Version
-           print td_list[2].text.strip() # App Summary
+           #print a_list[0].text # App Name
+           #print a_list[1].text # App Version
+           #print td_list[2].text.strip() # App Summary
+           app_name = a_list[0].text.strip().encode('UTF-8') # App Name
+           app_version = a_list[1].text.strip().encode('UTF-8') # App Version
+           app_summary = td_list[2].text.strip().encode('UTF-8') # App Summary
+	   app_list.append((app_name,app_version,app_summary,app_category))
         #if td_list[0].table != None:
             #print app_soup[i].td.table.tr.td.find_all('a')
             #a_list = app_soup[i].td.table.tr.td.find_all('a')
@@ -28,7 +37,15 @@ for i in range(0, len(app_soup)):
             #print a_list[1].text # App Version
     #if i > 1:
     #    break;
-            
+
+#print app_list
+#print len(app_list) # 54
+#print sql_app_id_lookup
+#print sql_app_id_lookup['Basic'] # 1
+#print sql_app_id_lookup['Miscellaneous'] # 9
+#print len(sql_app_id_lookup) # 9
+
+"""
 #print app_soup[1]
 #for i in range(0, len(app_soup)):
 #    print app_soup[i]
@@ -99,4 +116,4 @@ for i in range(0, len(soup.contents)):
 #print(soup.contents[3])
 #tag name
 #print soup.contents[4].name
-
+"""

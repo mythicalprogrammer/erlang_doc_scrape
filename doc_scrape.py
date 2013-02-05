3f5b3e4ae4a308b0786d5947504e3eceaac871e3
Anthony Doan <mythicalprogrammer@gmail.com>
2013-02-04 22:06:13 -0800

    Scrape app webpage

--- doc_scrape.py
+++ doc_scrape.py
@@ -1,4 +1,39 @@
 from bs4 import BeautifulSoup
+import os
+
+#app_list
+#app_list = os.listdir('otp_doc_html_R16A_RELEASE_CANDIDATE/lib')
+#app_list = [x.split('-')[0] for x in app_list]
+
+app_list = open('otp_doc_html_R16A_RELEASE_CANDIDATE/doc/applications.html','r')
+app_soup = BeautifulSoup(app_list).find_all('tr')
+app_cat = ''
+for i in range(0, len(app_soup)):
+    if len(app_soup[i].attrs) == 0:
+        if len(app_soup[i].td.attrs) == 2:
+            app_cat = app_soup[i].td.font.b.text # App Category
+   #         print app_soup[i].td.font.b.text # App Category
+    elif len(app_soup[i].attrs) == 1:
+        td_list = app_soup[i].find_all('td')
+        if len(td_list) == 3:
+           print app_cat
+           a_list = td_list[1].find_all('a')
+           print a_list[0].text # App Name
+           print a_list[1].text # App Version
+           print td_list[2].text.strip() # App Summary
+        #if td_list[0].table != None:
+            #print app_soup[i].td.table.tr.td.find_all('a')
+            #a_list = app_soup[i].td.table.tr.td.find_all('a')
+            #print a_list[0].text # App Name
+            #print a_list[1].text # App Version
+    #if i > 1:
+    #    break;
+            
+#print app_soup[1]
+#for i in range(0, len(app_soup)):
+#    print app_soup[i]
+#    if i > 2:
+#        break;
 
 lists = open("lib/stdlib-1.19/doc/html/lists.html", "r")
 soup = BeautifulSoup(lists)
